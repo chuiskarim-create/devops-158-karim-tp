c86de6771092d3f844300b2abfd4c557ff759492
chuiskarim <chuiskarim@gmail.com>
2026-05-11 11:48:50 +0200

    Modification du message Hello World

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-return "Hello EPSIC 158! - Modifié automatiquement par Jenkins !"
+return "Hello blablabla!"
 if __name__ == '__main__' :
 	app.run(host='0.0.0.0', port=5000)
